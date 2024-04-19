50b0766bd984d08ada3677a051b1b2b9ad9aaa28
nqianhuiii <nghui@graduate.utm.my>
2024-04-19 18:47:37 +0800

    create branch

--- lib/Discover/discover.dart
+++ lib/Discover/discover.dart
@@ -5,7 +5,7 @@ class DiscoverPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Discover')),
-      body: Center(
+      body: const Center(
         child: Text('Discover Page'),
       ),
     );
