8d94451aa514ba26e68849b2f511d4998c71eb39
nqianhuiii <nghui@graduate.utm.my>
2024-04-21 16:10:02 +0800

    merge master

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\jessm\OneDrive\Desktop\work\MAP\fitness_tracking"
+export "FLUTTER_ROOT=/Applications/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ngqianhui/Documents/GitHub/fitness-tracking"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
